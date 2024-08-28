c21d9a9d00f29b87dc05da77dc67f2065831c59b
jono1231 <jonwang2015@gmail.com>
2024-08-28 07:10:30 +0200

    Implemented solve and reset

--- bot.js
+++ bot.js
@@ -29,55 +29,30 @@ discord.on('messageCreate', async msg => {
     const user = discord.users.cache.get(msg.author.id);
     mquery = mquery.substring(1);
 
-    if (mquery === "start" && mcontent === process.env.FLAG) {
+    console.log(mquery + " " + mcontent);
+
+    if (mquery === "solve" && mcontent === process.env.FLAG) {
       // TODO: Give solved role when I can figure that out
+      console.log(mcontent);
+      msg.reply("Congrats!");
     }
 
     if (mquery === "start") {
-      // Build the AI helper
-      const userThread = await openai.beta.threads.create()
-      const message = await openai.beta.threads.messages.create(userThread.id,
-        {
-          role : "user",
-          content : "Let's begin the adventure!"
-        }
-      );
-      // Start the challenge
-      // Create the valid channel
-      channel = msg.guild.channels.create({
-        name: user.globalName,
-        type: ChannelType.GuildText,
-        parent: msg.channel.parent,
-        permissionOverwrites: [
-          {
-            id: msg.guild.roles.everyone,
-            deny: [PermissionFlagsBits.ViewChannel],
-          },
-          {
-            id: msg.author.id,
-            allow: [PermissionFlagsBits.ViewChannel],
-          },
-          {
-            id: discord.user.id,
-            allow: [PermissionFlagsBits.ViewChannel],
-          },
-        ],
-      }).then(channel => {
-        // Afterwards, get the Discord run used
-        channel.send("Welcome! Please wait as we generate your adventure...");
-        getRun(userThread.id, channel);
+      if (activeChannels.has(msg.author.id)) {
+        // TODO: Implement some error checking
+      } else {
+        await start(msg, user);
+      }
+    }
 
-        // Store the user's thread ID
-        activeThreads.set(user.id , userThread.id);
-        activeChannels.set(channel.id, user.id);
-        // console.log("Stored " + channel.id + " " + user.id);
-
-        fs.writeFile(LOG_FILE, `${user.id} ${userThread.id} ${channel.id}`, (err) => {if (err){ throw err }});
-      });
+    if (mquery === "reset") {
+      await reset(user);
     }
 
     msg.delete();
-  } else if (activeChannels.has(msg.channel.id) && activeThreads.has(msg.author.id)) {
+
+  } else if (activeChannels.has(msg.author.id) &&
+              activeChannels.get(msg.author.id).id === msg.channel.id && activeThreads.has(msg.author.id)) {
     // Very "hacky" solution but fuck it we ball I guess
     // Check if the message was sent in an active channel
     // console.log(msg.channel.id + " " + msg.author.id);
@@ -101,12 +76,77 @@ discord.on('messageCreate', async msg => {
   // console.log(msg.channel.id + " " + msg.author.id)
 });
 
-// TODO: Give role on join when I can
-discord.on('guildMemberAdd', async member => {
-  console.log('User ' + member.user.username + 'has joined the server!') 
-  let role = member.guild.roles.cache.find(x => x.name == 'Adventurers'); 
-  member.roles.add(role)
-});
+// // TODO: Give role on join when I can
+// discord.on('guildMemberAdd', async member => {
+//   console.log('User ' + member.user.username + 'has joined the server!') 
+//   let role = member.guild.roles.cache.find(x => x.name == 'Adventurers'); 
+//   member.roles.add(role)
+// });
+
+async function start(msg, user) {
+  // console.log("Start triggered!");
+  // Build the AI helper
+  const userThread = await openai.beta.threads.create()
+  const message = await openai.beta.threads.messages.create(userThread.id,
+    {
+      role : "user",
+      content : "Let's begin the adventure!"
+    }
+  );
+  // Start the challenge
+  // Create the valid channel
+  channel = msg.guild.channels.create({
+    name: user.globalName,
+    type: ChannelType.GuildText,
+    parent: msg.channel.parent,
+    permissionOverwrites: [
+      {
+        id: msg.guild.roles.everyone,
+        deny: [PermissionFlagsBits.ViewChannel],
+      },
+      {
+        id: msg.author.id,
+        allow: [PermissionFlagsBits.ViewChannel],
+      },
+      {
+        id: discord.user.id,
+        allow: [PermissionFlagsBits.ViewChannel],
+      },
+    ],
+  }).then(channel => {
+    channel.setRateLimitPerUser(5, "Don't fuck up");
+    // console.log("Channel created");
+    // Afterwards, get the Discord run used
+    channel.send("Welcome! Please wait as we generate your adventure...");
+    getRun(userThread.id, channel);
+
+    // Store the user's thread ID
+    activeThreads.set(user.id , userThread.id);
+    activeChannels.set(user.id, channel);
+    // console.log("Stored " + channel.id + " " + user.id);
+  });
+}
+
+async function reset(user) {
+  id = user.id;
+  if (activeThreads.has(user.id)) {
+    channel = activeChannels.get(user.id);
+    
+    channel.send("Resetting the adventure... Please wait");
+
+    const userThread = await openai.beta.threads.create()
+    const message = await openai.beta.threads.messages.create(userThread.id,
+      {
+        role : "user",
+        content : "Let's begin the adventure!"
+      }
+    );
+
+    getRun(userThread.id, channel);
+
+    activeThreads.set(user.id , userThread.id);
+  }
+}
 
 async function getRun(thread_id, channel) {
   // Can't have it reply by username because it breaks the whole AI for no fucking reason
